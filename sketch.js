cda869f737fa7cad493d14a4cfb53a704014f179
Sean Martin <sean@seanmartinstudio.com>
2023-08-04 11:45:26 -0700

    initial commit

---
+++ sketch.js
@@ -0,0 +1,32 @@
+let unitsX = 45
+let unitsY = 35
+let numBox = 6
+
+function setup() {
+  createCanvas(800, 800, WEBGL)
+}
+
+function keyPressed() {
+  // download gif
+  if (key === 's') {
+    saveGif('myGIF', 10)
+  }
+}
+
+function draw() {
+  background(0)
+  frameRate(10)
+
+  for (let i = 0; i < numBox; i++) {
+
+    unitsX += 2
+    unitsY += 2
+
+    fill(random(0, 255), random(0, 255), random(0, 255))
+    stroke(random(0, 255), random(0, 255), random(0, 255))
+    rotateX(radians(unitsX))
+    rotateY(radians(unitsY))
+    box(175, 175, 175)
+  }
+
+}
